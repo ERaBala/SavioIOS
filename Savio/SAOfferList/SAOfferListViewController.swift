6557f78349d8feb2a206e843360e96706a7dcced
Pavan <pavan.p@ardor.co>
2016-06-06 18:37:13 +0530

    Started working on offer list screen

--- Savio/SAOfferList/SAOfferListViewController.swift
+++ Savio/SAOfferList/SAOfferListViewController.swift
@@ -1,14 +1,14 @@
 //
-//  SAWishListViewController.swift
+//  SAOfferListViewController.swift
 //  Savio
 //
-//  Created by Prashant on 04/06/16.
+//  Created by Prashant on 06/06/16.
 //  Copyright © 2016 Prashant. All rights reserved.
 //
 
 import UIKit
 
-class SAWishListViewController: UIViewController {
+class SAOfferListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,14 +17,19 @@ class SAWishListViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     func setUpView(){
-        self.title = "My Wish List"
+        self.title = "Partner offers"
         
         //set Navigation left button
         let leftBtnName = UIButton()
-        leftBtnName.setImage(UIImage(named: "nav-menu.png"), forState: UIControlState.Normal)
+        leftBtnName.setImage(UIImage(named: "nav-back.png"), forState: UIControlState.Normal)
         leftBtnName.frame = CGRectMake(0, 0, 30, 30)
-        leftBtnName.addTarget(self, action: #selector(SACreateSavingPlanViewController.menuButtonClicked), forControlEvents: .TouchUpInside)
+        leftBtnName.addTarget(self, action: #selector(SAOfferListViewController.backButtonPress), forControlEvents: .TouchUpInside)
         
         let leftBarButton = UIBarButtonItem()
         leftBarButton.customView = leftBtnName
@@ -38,19 +43,32 @@ class SAWishListViewController: UIViewController {
         btnName.titleLabel!.font = UIFont(name: "GothamRounded-Book", size: 12)
         btnName.setTitle("0", forState: UIControlState.Normal)
         btnName.setTitleColor(UIColor(red: 0.94, green: 0.58, blue: 0.20, alpha: 1), forState: UIControlState.Normal)
-        btnName.addTarget(self, action: #selector(SACreateSavingPlanViewController.heartBtnClicked), forControlEvents: .TouchUpInside)
+        btnName.addTarget(self, action: #selector(SAOfferListViewController.heartBtnClicked), forControlEvents: .TouchUpInside)
         
         let rightBarButton = UIBarButtonItem()
         rightBarButton.customView = btnName
         self.navigationItem.rightBarButtonItem = rightBarButton
     }
+    /*
+    // MARK: - Navigation
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    // MARK: - Navigation Bar Button Action
+    func backButtonPress()  {
+        self.navigationController?.popViewControllerAnimated(true)
     }
     
-     // MARK: - Tableview Delegate & Datasource
+    func heartBtnClicked()  {
+        print("Heart Btn Clicked..")
+    }
+    
+    
+    // MARK: - Tableview Delegate & Datasource
     func numberOfSectionsInTableView(tableView: UITableView) -> Int  {
         return 1;
     }
@@ -62,35 +80,26 @@ class SAWishListViewController: UIViewController {
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
         //        let cell = tableView.dequeueReusableCellWithIdentifier("SavingCategoryTableViewCell") as? SavingCategoryTableViewCell
         //        if cell == nil {
-        let bundleArr : Array = NSBundle.mainBundle().loadNibNamed("WishListTableViewCell", owner: nil, options: nil) as Array
-        let cell = bundleArr[0] as! WishListTableViewCell
+        let bundleArr : Array = NSBundle.mainBundle().loadNibNamed("SAOfferListTableViewCell", owner: nil, options: nil) as Array
+        let cell = bundleArr[0] as! SAOfferListTableViewCell
         //        }
-//        let cellDict = tblArr[indexPath.row]
-//        print(cellDict["header"])
-//        cell.lblHeader!.text = cellDict["header"] as? String;
-//        cell.lblDetail?.text = cellDict["detail"] as? String
-//        cell.imgView?.image = UIImage(named: cellDict["image"] as! String)
+        //        let cellDict = tblArr[indexPath.row]
+        //        print(cellDict["header"])
+        //        cell.lblHeader!.text = cellDict["header"] as? String;
+        //        cell.lblDetail?.text = cellDict["detail"] as? String
+        //        cell.imgView?.image = UIImage(named: cellDict["image"] as! String)
         
-        cell.btnSavingPlan?.addTarget(self, action: #selector(SAWishListViewController.navigateToSetUpSavingPlan(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        cell.btnAddOffer?.addTarget(self, action: #selector(SAOfferListViewController.clickedOnAddOffer(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         return cell
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 310.0
     }
-
-    func navigateToSetUpSavingPlan(sender:UIButton) {
+    
+    func clickedOnAddOffer(sender:UIButton) {
         print("Clicked on Wishlist button")
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
