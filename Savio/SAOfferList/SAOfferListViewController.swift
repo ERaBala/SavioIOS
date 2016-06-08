e69fda61f694bce02f16b790e8dd1173df346262
Pavan <pavan.p@ardor.co>
2016-06-08 12:24:22 +0530

    Done Expandable Cell

--- Savio/SAOfferList/SAOfferListViewController.swift
+++ Savio/SAOfferList/SAOfferListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SAOfferListViewController: UIViewController {
     var indx : Int = 0
+    var  prevIndxArr: Array<Int> = []
+    
     var rowHT : CGFloat = 310.0
     
     @IBOutlet weak var tblView : UITableView?
@@ -73,7 +75,7 @@ class SAOfferListViewController: UIViewController {
     
     // MARK: - Tableview Delegate & Datasource
     func numberOfSectionsInTableView(tableView: UITableView) -> Int  {
-        return 1;
+        return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
@@ -86,32 +88,47 @@ class SAOfferListViewController: UIViewController {
         let bundleArr : Array = NSBundle.mainBundle().loadNibNamed("SAOfferListTableViewCell", owner: nil, options: nil) as Array
         let cell = bundleArr[0] as! SAOfferListTableViewCell
    
-        
         cell.btnAddOffer?.addTarget(self, action: #selector(SAOfferListViewController.clickedOnAddOffer(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         cell.btnOfferDetail?.addTarget(self, action: #selector(SAOfferListViewController.clickedOnOfferDetail(_:)), forControlEvents: UIControlEvents.TouchUpInside)
         cell.btnOfferDetail?.tag = indexPath.row
-        if indx == indexPath.row {
-             let str = "This is Savio application and team size is 4, name: Prashant, Maheshwari, Manoj and Gagan"
-            let ht = self.heightForView(str, font: UIFont(name: "GothamRounded-Book", size: 10)!, width: (cell.lblProductDetail?.frame.size.width)! )
-            cell.lblHT.constant = ht
-            cell.lblProductDetail?.text = str
+        
+        if prevIndxArr.count > 0 {
+            var ht: CGFloat = 0.0
+            var str = ""
+            
+
+            for var i in 0 ..< prevIndxArr.count {
+                if prevIndxArr[i] == indexPath.row {
+                     str = "This is Savio application and team size is 4, name: Prashant, Maheshwari, Manoj and Gagan"
+                    ht = self.heightForView(str, font: UIFont(name: "GothamRounded-Book", size: 10)!, width: (cell.lblProductDetail?.frame.size.width)! )
+                }
+                else{
+                     str = ""
+                     ht = 0.0
+                }
+                cell.lblHT.constant = ht
+                cell.lblProductDetail?.text = str
+            }
+            
+           
         }
         else{
             cell.lblHT.constant = 0.0
             cell.lblProductDetail?.text = ""
         }
-
 //        cell.lblHT.constant = 20.0
         return cell
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        
-        if indexPath.row == indx{
-            return rowHT + 310.0
-        }else{
-        return 310.0
+        if prevIndxArr.count > 0 {
+            for var i in 0 ..< prevIndxArr.count {
+                if prevIndxArr[i] == indexPath.row {
+                    return rowHT + 310.0
+                }
+            }
         }
+        return 310.0
     }
     
     func clickedOnAddOffer(sender:UIButton) {
@@ -121,8 +138,26 @@ class SAOfferListViewController: UIViewController {
     func clickedOnOfferDetail(sender:UIButton) {
         print(sender.tag)
         indx = sender.tag
+        var isVisible = false
+        if prevIndxArr.count > 0{
+            for i in 0 ..< prevIndxArr.count {
+               let obj = prevIndxArr[i] as Int
+                if obj == sender.tag{
+                  isVisible = true
+                    prevIndxArr.removeAtIndex(i)
+                    break
+                }
+            }
+            
+            if(isVisible == false){
+                prevIndxArr.removeAll()
+                prevIndxArr.append(sender.tag)
+            }
+        }
+        else{
+            prevIndxArr.append(sender.tag)
+        }
         tblView?.reloadData()
-       
     }
 
     func heightForView(text:String, font:UIFont, width:CGFloat) -> CGFloat{
